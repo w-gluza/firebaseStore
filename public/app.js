026819b3f9eaefc1bc92ea7719349146ce473a18
Wioletta Gluza <wiolagluza@gmail.com>
2019-09-29 00:46:01 +0200

    first commit

---
+++ public/app.js
@@ -0,0 +1,98 @@
+console.log('hello');
+
+// function updateProduct(e) {
+//   const db = firebase.firestore();
+//   const myProduct = db.collection('products').doc('mdbKuTKmJWRgevwMEt6J');
+//   myProduct.update({ name: e.target.value });
+// }
+
+// document.addEventListener('DOMContentLoaded', event => {
+//   const app = firebase.app();
+//   const db = firebase.firestore();
+//   const myProducts = db.collection('products');
+
+//   const productsContainer = document.querySelector('#products__container');
+//   const docFrag = document.createDocumentFragment();
+
+//   let productName = document.createElement('h4');
+//   let productPrice = document.createElement('p');
+
+//   docFrag.appendChild(productName);
+//   docFrag.appendChild(productPrice);
+
+//   productsContainer.appendChild(docFrag);
+
+document.addEventListener('DOMContentLoaded', event => {
+  const db = firebase.firestore();
+  const myProducts = db.collection('products');
+  const productsContainer = document.querySelector('#products__container');
+  const form = document.querySelector('#add__product');
+
+  function renderProduct(doc) {
+    const docFrag = document.createDocumentFragment();
+    let article = document.createElement('article');
+    let productName = document.createElement('h4');
+    let productPrice = document.createElement('p');
+    let deleteButton = document.createElement('button');
+    let updateButton = document.createElement('button');
+
+    article.setAttribute('product-id', doc.id);
+
+    productName.textContent = doc.data().name;
+    productPrice.textContent = doc.data().price;
+    deleteButton.textContent = 'Delete';
+    updateButton.textContent = 'Update';
+
+    docFrag.appendChild(productName);
+    docFrag.appendChild(productPrice);
+    docFrag.appendChild(deleteButton);
+    docFrag.appendChild(updateButton);
+
+    article.appendChild(docFrag);
+    productsContainer.appendChild(article);
+
+    deleteButton.addEventListener('click', e => {
+      let id = e.target.parentElement.getAttribute('product-id');
+      myProducts.doc(id).delete();
+    });
+
+    updateButton.addEventListener('click', e => {
+      let id = e.target.parentElement.getAttribute('product-id');
+      myProducts.doc(id).update({
+        name: 'test',
+        price: '999'
+      });
+    });
+  }
+
+  // getting data from Firestore
+
+  myProducts.onSnapshot(products => {
+    let changes = products.docChanges();
+    changes.forEach(change => {
+      if (change.type === 'added') {
+        renderProduct(change.doc);
+      } else if (change.type === 'removed') {
+        let article = productsContainer.querySelector(
+          '[product-id=' + change.doc.id + ']'
+        );
+        productsContainer.removeChild(article);
+      } else if (change.type === 'modified') {
+        let article = productsContainer.querySelector(
+          '[product-id=' + change.doc.id + ']'
+        );
+        productsContainer.removeChild(article);
+        renderProduct(change.doc);
+      }
+    });
+  });
+
+  // creating new data
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    myProducts.add({
+      name: form.name.value,
+      price: form.price.value
+    });
+  });
+});
