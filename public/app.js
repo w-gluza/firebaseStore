efb97148f61156aef15794bdcbeb2ffb8d4027bb
Wioletta Gluza <wiolagluza@gmail.com>
2019-09-29 13:22:34 +0200

    user auth status

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', event => {
   const db = firebase.firestore();
+  const auth = firebase.auth();
   const myProducts = db.collection('products');
   const productsContainer = document.querySelector('#products__container');
   const form = document.querySelector('#add__product');
@@ -71,4 +72,47 @@ document.addEventListener('DOMContentLoaded', event => {
       price: form.price.value
     });
   });
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log('user logged in: ', user);
+    } else {
+      console.log('user logged out: ');
+    }
+  });
+
+  const signupForm = document.querySelector('#signup-form');
+  signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up user
+    const auth = firebase.auth();
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      console.log(cred);
+    });
+  });
+  // log out form
+  const logout = document.querySelector('#logout');
+  logout.addEventListener('click', e => {
+    const auth = firebase.auth();
+    e.preventDefault();
+    auth.signOut();
+  });
+
+  //log in
+  const loginForm = document.querySelector('#login-form');
+  loginForm.addEventListener('submit', e => {
+    const auth = firebase.auth();
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+      console.log('user logged in');
+    });
+  });
+
+  // end document loaded
 });
